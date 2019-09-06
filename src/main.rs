aab3f2994caed41a4be1865fcad01549277ae38d
Andrew Martin <0andrewmartin0@gmail.com>
2019-09-06 20:06:10 +0100

    checkers + basic AI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 
 use std::ops::Add;
 use std::fmt;
+use rand::{Rng, SeedableRng, rngs::StdRng};
 
 #[derive(Copy, Clone, Debug)]
 struct Pos {
@@ -16,6 +17,7 @@ impl Add for Pos {
   }
 }
 
+/// Holds the contents of a tile
 #[derive(Copy, Clone, PartialEq)]
 enum Tile {
   White,
@@ -25,16 +27,34 @@ enum Tile {
   Empty,
 }
 
+/// The two competing players
 #[derive(Copy, Clone, PartialEq)]
 enum Player {
   WhitePlayer,
   BlackPlayer,
 }
 
+/// The mode that the game is in.
+#[derive(Copy, Clone)]
+enum Mode {
+  /// Active player can choose any move
+  StartOfTurn,
+
+  /// This exists to cope with the fact that players must
+  /// sometimes take multiple actions in one turn; if they
+  /// capture a piece, they must continue using the same
+  /// piece to capture any other pieces available.
+  Chain(Pos),
+
+  /// One of the players won
+  Victory(Player),
+}
+
 use Tile::*;
 use Player::*;
 
 impl Tile {
+  /// Indicates which player the tile belongs to (if either)
   fn player(self) -> Option<Player> {
     match self {
       White | WhiteKing => Some(WhitePlayer),
@@ -44,8 +64,11 @@ impl Tile {
   }
 }
 
+/// The size of the board (which is assumed to be square)
 const BOARD_SIZE : i32 = 8;
 
+/// Stores the tiles. This is separated out to add convenient
+/// access methods, and so it can be optimised later.
 #[derive(Clone)]
 struct Board {
   tiles : [Tile ; (BOARD_SIZE * BOARD_SIZE) as usize]
@@ -65,29 +88,30 @@ impl Board {
       None
     }
     else {
-      Some(self.get(p.x, p.y))
+      Some(self.get(p))
     }
   }
 
-  fn get(&self, x : i32, y : i32) -> Tile {
-    self.tiles[self.index(x, y)]
+  fn get(&self, p : Pos) -> Tile {
+    self.tiles[self.index(p.x, p.y)]
   }
 
-  fn get_mut(&mut self, x : i32, y : i32) -> &mut Tile {
-    &mut self.tiles[self.index(x, y)]
+  fn set(&mut self, p : Pos, t : Tile) {
+    self.tiles[self.index(p.x, p.y)] = t;
   }
 }
 
-#[derive(Debug)]
-enum Move {
-  Simple { from : Pos, to : Pos },
-  Capture { from : Pos, capture : Pos, then : Box<Move> },
+#[derive(Debug, Copy, Clone)]
+enum Action {
+  Step { from : Pos, to : Pos },
+  Jump { from : Pos, capture : Pos, to : Pos },
 }
 
 #[derive(Clone)]
 struct GameState {
   board : Board,
-  turn : Player,
+  active_player : Player,
+  mode : Mode,
 }
 
 impl GameState {
@@ -97,20 +121,20 @@ impl GameState {
     for y in 0..3 {
       for i in (0..BOARD_SIZE).step_by(2) {
         let x = i + (y % 2);
-        *board.get_mut(x, y) = White;
+        board.set(Pos {x, y}, White);
       }
     }
     for y in (BOARD_SIZE-3)..BOARD_SIZE {
       for i in (0..BOARD_SIZE).step_by(2) {
         let x = i + (y % 2);
-        *board.get_mut(x, y) = Black;
+        board.set(Pos{x, y}, Black);
       }
     }
-    GameState { board, turn : WhitePlayer }
+    GameState { board, active_player : WhitePlayer, mode: Mode::StartOfTurn }
   }
 
-  fn moves_from_pos(&self, start : Pos, moves : &mut Vec<Move>) {
-    let start_tile = self.board.get(start.x, start.y);
+  fn moves_from_pos(&self, start : Pos, steps : &mut Vec<Action>, jumps : &mut Vec<Action>) {
+    let start_tile = self.board.get(start);
     let possible_moves : &[Pos] = match start_tile {
       White => &[Pos {x: -1, y: 1}, Pos {x: 1, y: 1}],
       Black => &[Pos {x: -1, y: -1}, Pos {x: 1, y: -1}],
@@ -125,29 +149,124 @@ impl GameState {
         match t.player() {
           Some(p) => {
             if p != start_tile.player().unwrap() {
-              let hop = pos + *m;
-              if let Some(Tile::Empty) = self.board.try_get(hop) {
-
+              let jump = pos + *m;
+              if let Some(Tile::Empty) = self.board.try_get(jump) {
+                jumps.push(Action::Jump{ from: start, capture: pos, to: jump});
               }
             }
           }
-          None => moves.push(Move::Simple{ from: start, to: pos}),
+          None => steps.push(Action::Step{ from: start, to: pos}),
         }
       }
     }
   }
 
-  fn possible_actions(&self) -> Vec<Move> {
-    let mut moves = vec![];
-    for (i, p) in self.board.tiles.iter().enumerate() {
-      if p.player() == Some(self.turn) {
-        let x = (i as i32) % BOARD_SIZE;
-        let y = (i as i32) / BOARD_SIZE;
-        let p = Pos{ x, y };
-        self.moves_from_pos(p, &mut moves);
+  fn possible_actions(&self) -> Vec<Action> {
+    let mut steps = vec![];
+    let mut jumps = vec![];
+    match self.mode {
+      Mode::StartOfTurn => {
+        for (i, p) in self.board.tiles.iter().enumerate() {
+          if p.player() == Some(self.active_player) {
+            let x = (i as i32) % BOARD_SIZE;
+            let y = (i as i32) / BOARD_SIZE;
+            let p = Pos{ x, y };
+            self.moves_from_pos(p, &mut steps, &mut jumps);
+          }
+        }
+      }
+      Mode::Chain(p) => {
+        self.moves_from_pos(p, &mut steps, &mut jumps);
+      }
+      Mode::Victory(_) => (),
+    }
+    if jumps.len() > 0 {
+      jumps
+    }
+    else {
+      steps
+    }
+  }
+
+  fn chain_check(&mut self, p : Pos) {
+    let mut steps = vec!();
+    let mut jumps = vec!();
+    self.moves_from_pos(p, &mut steps, &mut jumps);
+    if jumps.len() > 0 {
+      self.mode = Mode::Chain(p);
+    }
+    else {
+      self.active_player_swap();
+      self.mode = Mode::StartOfTurn;
+    }
+  }
+
+  fn king_check(&mut self, p : Pos) {
+    let tile_value = self.board.get(p);
+    match tile_value {
+      Tile::Black => {
+        if p.y == 0 {
+          self.board.set(p, Tile::BlackKing);
+        }
+      }
+      Tile::White => {
+        if p.y == BOARD_SIZE-1 {
+          self.board.set(p, Tile::WhiteKing);
+        }
+      }
+      _ => (),
+    }
+  }
+
+  fn victory_check(&self) -> bool {
+    let (white, black) = self.piece_count();
+    white == 0 || black == 0
+  }
+
+  fn piece_count(&self) -> (i32, i32) {
+    let mut white = 0;
+    let mut black = 0;
+    for tile in self.board.tiles.iter() {
+      match tile {
+        Tile::Black => black += 1,
+        Tile::BlackKing => black += 2,
+        Tile::White => white += 1,
+        Tile::WhiteKing => white += 2,
+        Tile::Empty => (),
       }
     }
-    moves
+    (white, black)
+  }
+
+  fn active_player_swap(&mut self) {
+    match self.active_player {
+      Player::WhitePlayer => self.active_player = Player::BlackPlayer,
+      Player::BlackPlayer => self.active_player = Player::WhitePlayer,
+    }
+  }
+
+  fn apply_action(&mut self, a : Action) {
+    match a {
+      Action::Step { from, to } => {
+        let tile_value = self.board.get(from);
+        self.board.set(from, Tile::Empty);
+        self.board.set(to, tile_value);
+        self.king_check(to);
+        self.active_player_swap();
+        self.mode = Mode::StartOfTurn;
+      }
+      Action::Jump { from, capture, to } => {
+        let tile_value = self.board.get(from);
+        self.board.set(from, Tile::Empty);
+        self.board.set(capture, Tile::Empty);
+        self.board.set(to, tile_value);
+        self.king_check(to);
+        self.chain_check(to);
+        if self.victory_check() {
+          self.mode = Mode::Victory(tile_value.player().unwrap());
+        }
+      }
+    }
   }
 }
 
@@ -156,7 +275,7 @@ impl fmt::Display for GameState {
     for y in 0..BOARD_SIZE {
       write!(f, "|")?;
       for x in 0..BOARD_SIZE {
-        let s = match self.board.get(x, y) {
+        let s = match self.board.get(Pos{x, y}) {
           Black => "b", BlackKing => "B",
           White => "w", WhiteKing => "W",
           Empty => " ",
@@ -169,14 +288,101 @@ impl fmt::Display for GameState {
   }
 }
 
-fn main() {
-  println!("Checkers!");
-  let mut game = GameState::new();
-  for a in game.possible_actions() {
-    if let Move::Simple{ from, to } = a {
-      *game.board.get_mut(from.x, from.y) = WhiteKing;
-      *game.board.get_mut(to.x, to.y) = BlackKing;
+use piston_window::*;
+
+fn rollout(game : &mut GameState, rng : &mut StdRng, max_depth : i32) -> f64 {
+  for _ in 0..max_depth {
+    let actions = game.possible_actions();
+    if actions.len() > 0 {
+      let i: usize = rng.gen_range(0, actions.len());
+      game.apply_action(actions[i]);
+    }
+    else {
+      break;
     }
   }
-  println!("{}", game);
+  let (white, black) = game.piece_count();
+  (white - black) as f64
 }
+
+fn choose_white_action(game : &GameState, rng : &mut StdRng) -> Option<Action> {
+  let actions = game.possible_actions();
+  let mut best_score = 0.0;
+  let mut best_action = None;
+  for a in actions {
+    let mut score = 0.0;
+    for _ in 0..10 {
+      let mut g = game.clone();
+      g.apply_action(a);
+      score += rollout(&mut g, rng, 10);
+    }
+    if score > best_score {
+      best_score = score;
+      best_action = Some(a);
+    }
+  }
+  best_action
+}
+
+fn main() {
+
+  println!("Checkers!");
+  let mut game = GameState::new();
+  let mut rng = StdRng::from_entropy(); //StdRng::seed_from_u64(0);
+
+  let mut window: PistonWindow =
+    WindowSettings::new("Checkers", [480, 480])
+    .exit_on_esc(true).build().unwrap();
+  while let Some(event) = window.next() {
+    if let Some(Button::Keyboard(key)) = event.press_args() {
+      if key == Key::Space {
+        let a =
+          if game.active_player == WhitePlayer {
+            choose_white_action(&mut game, &mut rng)
+          }
+          else {
+            let actions = game.possible_actions();
+            if actions.len() > 0 {
+              let i: usize = rng.gen_range(0, actions.len());
+              Some(actions[i])
+            }
+            else {
+              None
+            }
+          };
+        if let Some(a) = a {
+          game.apply_action(a);
+        }
+      }
+    }
+    window.draw_2d(&event, |context, graphics, _device| {
+      clear([1.0; 4], graphics);
+      for y in 0..BOARD_SIZE {
+        for i in (0..BOARD_SIZE).step_by(2) {
+          let x = i + (y % 2);
+          rectangle(
+            [0.0, 0.0, 0.0, 1.0], // black
+            [x as f64 * 60.0, y as f64 * 60.0, 60.0, 60.0],
+            context.transform,
+            graphics);
+
+          let tile = game.board.get(Pos{x, y});
+          let colour = match tile {
+            Tile::Black => Some([1.0, 0.0, 0.0, 1.0]),
+            Tile::White => Some([0.0, 1.0, 0.0, 1.0]),
+            Tile::BlackKing => Some([0.5, 0.0, 0.0, 1.0]),
+            Tile::WhiteKing => Some([0.0, 0.5, 0.0, 1.0]),
+            Empty => None,
+          };
+          if let Some(c) = colour {
+            ellipse(
+              c, [x as f64 * 60.0 + 5.0, y as f64 * 60.0 + 5.0, 50.0, 50.0],
+              context.transform,
+              graphics);
+          }
+        }
+      }
+    });
+  }
+}
+
